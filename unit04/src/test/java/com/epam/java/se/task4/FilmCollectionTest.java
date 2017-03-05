fdcabd9450ad04c2a9b840b3f34952a4047e84fd
YuriJRC[credential] <Jurassic5@ya.ru>
2017-03-05 13:21:52 +0300

    task4 - final test (application logic) added

--- unit04/src/test/java/com/epam/java/se/task4/FilmCollectionTest.java
+++ unit04/src/test/java/com/epam/java/se/task4/FilmCollectionTest.java
@@ -99,15 +99,47 @@ public class FilmCollectionTest {
             }
         }
     }
-//        for (Map.Entry<Film, List<Actor>> entry : after.getFilmCollection().entrySet()) {
-//
-//            System.out.println(entry.getKey().getTitle());
-//            assertTrue(entry.getKey().getTitle().equals(film));
-//        }
-//    }
 
     @Test
-    public void applicationTest() throws Exception {
+    public void applicationLogicTest() throws Exception {
+        FilmCollection first = new FilmCollection();
 
+        first.addFilm(new Film("Lord of the rings", "fantasy", 2005),
+                new Actor("aaa"), new Actor("bbb"), new Actor("ccc"));
+        first.addFilm(new Film("Titanic", "drama", 1998),
+                new Actor("Leo Dicaprio"), new Actor("Kate Winslet"));
+        first.addFilm(new Film("Dogma", "fantasy, drama", 1999),
+                new Actor("Matt Damon"), new Actor("Ben Affleck"), new Actor("Salma Hayek"));
+
+        first.saveCollection(PATH, first);
+
+        FilmCollection second = first.loadCollection(PATH);
+
+        second.addFilm((new Film("V for Vendetta", "thriller", 2005)),
+                new Actor("V"), new Actor("Z"), new Actor("X"), new Actor("Y"));
+
+        second.removeFilmByName("Lord of the rings");
+
+        second.saveCollection(PATH, second);
+
+        FilmCollection third = second.loadCollection(PATH);
+
+        third.printFilmCollection();
+
+        String film1 = "Dogma";
+        String film2 = "Titanic";
+        String film3 = "V for Vendetta";
+
+        Iterator<Map.Entry<Film, List<Actor>>> iter = third.getFilmCollection().entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Film, List<Actor>> entry = iter.next();
+            if (entry.getKey().getTitle().equalsIgnoreCase(film1) ||
+                    entry.getKey().getTitle().equalsIgnoreCase(film2) ||
+                    entry.getKey().getTitle().equalsIgnoreCase(film3)) {
+                assertTrue(entry.getKey().getTitle().equals(film1) ||
+                        entry.getKey().getTitle().equals(film2) ||
+                        entry.getKey().getTitle().equals(film3));
+            }
+        }
     }
 }
