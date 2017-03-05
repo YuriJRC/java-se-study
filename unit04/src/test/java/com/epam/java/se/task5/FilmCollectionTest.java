6643533b227f2ee1afc0f358aa41651987a93b44
YuriJRC[credential] <Jurassic5@ya.ru>
2017-03-05 11:52:24 +0300

    task4 - test added

--- unit04/src/test/java/com/epam/java/se/task5/FilmCollectionTest.java
+++ unit04/src/test/java/com/epam/java/se/task5/FilmCollectionTest.java
@@ -11,23 +11,36 @@ import org.junit.Test;
 public class FilmCollectionTest {
     private static final String PATH = "films_collection.ser";
     @Test
-    public void addFilmTest(){
+            (expected = NullPointerException.class)
+    public void nullPointerTest()throws Exception{
         FilmCollection films = new FilmCollection();
-        films.addFilm(new Film("Titanic", "drama", 1998),
-                new Actor("Leo Dicaprio"), new Actor("Kate Winslet"));
-        films.addFilm(new Film("Lord of the rings", "fantasy", 2005),
-                new Actor("aaa"), new Actor("bbb"), new Actor("ccc"));
-
+        films.addFilm(null);
+        films.removeFilmByName(null);
         films.printFilmCollection();
+        films.saveCollection(null, null);
+        films.loadCollection(null);
+    }
 
-        films.removeFilmByName("titanic");
-
-        films.printFilmCollection();
+//    @Test
+//    public void addFilmTest(){
+//        FilmCollection films = new FilmCollection();
+//        films.addFilm(new Film("Titanic", "drama", 1998),
+//                new Actor("Leo Dicaprio"), new Actor("Kate Winslet"));
+//        films.addFilm(new Film("Lord of the rings", "fantasy", 2005),
+//                new Actor("aaa"), new Actor("bbb"), new Actor("ccc"));
+//        films.addFilm(new Film("Dogma", "fantasy, drama", 1999),
+//                new Actor("Matt Damon"), new Actor("Ben Affleck"), new Actor("Salma Hayek"));
+//
+//        films.printFilmCollection();
+//
+//        films.removeFilmByName("titanic");
+//
+//        films.printFilmCollection();
 //        films.printFilmCollection();
 //        films.saveCollection(PATH, films);
 
 //        FilmCollection collection = films.loadCollection(PATH);
 //        collection.printFilmCollection();
 
-    }
+//    }
 }
