b62fedab8bbf9cb2d3d21d336363c0662745f75f
YuriJRC[credential] <Jurassic5@ya.ru>
2017-03-06 12:41:42 +0300

    new methods and tests added

--- unit05/src/main/java/com/epam/java/se/task1/FileWorker.java
+++ unit05/src/main/java/com/epam/java/se/task1/FileWorker.java
@@ -7,37 +7,36 @@ import java.io.*;
  */
 public class FileWorker {
 
-    public void createNewFile(File file) {
+    public void createNewFile(File file) throws IOException {
         try {
             if (file.createNewFile()) {
                 System.out.println("File created");
-            } else{
-                System.out.println("File already exists");
-            }
-        } catch (IOException e){
+            } else System.out.println("File already exists");
+        } catch (IOException e) {
             System.out.println(("Wrong input data"));
         }
     }
 
-    public void removeFile(){
-
+    public void deleteFile(File file) throws IOException {
+        if (file.delete()) {
+            System.out.println("File deleted");
+        } else throw new IOException("File not found");
     }
 
-    public void writeToFile(File inputFile, String text){
+    public void writeToFile(File inputFile, String text) throws FileNotFoundException {
         try {
             if (inputFile.exists()) {
                 PrintWriter writer = new PrintWriter(new FileOutputStream(inputFile, true));
                 writer.print(text);
                 writer.flush();
                 writer.close();
-            }
-            else System.out.println("File noy found");
-        } catch (FileNotFoundException e){
+            } else System.out.println("File not found");
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
     }
 
-    public void readFromFile(){
+    public void readFromFile() {
 
     }
 
--- unit05/src/test/java/com/epam/java/se/task1/FileWorkerTest.java
+++ unit05/src/test/java/com/epam/java/se/task1/FileWorkerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -14,16 +15,34 @@ public class FileWorkerTest {
     @Test
     public void NullPointerExceptionCaughtTest() throws Exception {
         File file = new File("C:\\Users\\fake\\fake");
+        String text = "Testtesttesttest";
+
+        FileWorker worker = new FileWorker();
+
+        try {
+            worker.writeToFile(file, text);
+        } catch (IOException e) {
+            assertEquals(e.getMessage(), "Wrong input data");
+        }
+    }
+
+    @Test
+    public void FileNotFoundExceptionCaughtTest() throws Exception {
+        File file = new File("C:\\Users\\fake\\fake");
 
         FileWorker worker = new FileWorker();
 
         try {
             worker.createNewFile(file);
-        } catch (NullPointerException e) {
+        } catch (IOException e) {
             assertEquals(e.getMessage(), "Wrong input data");
         }
 
-
+        try {
+            worker.deleteFile(file);
+        } catch (IOException e) {
+            assertEquals(e.getMessage(), "File not found");
+        }
     }
 
     @Test
@@ -35,6 +54,7 @@ public class FileWorkerTest {
 
         assertTrue(file.exists());
     }
+
     @Test
     public void writeToFileTest() throws Exception {
         File file = new File(".\\testfile4.txt");
@@ -46,4 +66,13 @@ public class FileWorkerTest {
 
         assertTrue(file.length()>10);
     }
+    @Test
+    public void deleteFileTest() throws Exception {
+        File file = new File(".\\testfile5.txt");
+
+        FileWorker worker = new FileWorker();
+
+        worker.deleteFile(file);
+    }
+
 }
