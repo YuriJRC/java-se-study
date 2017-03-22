00f952c376a66ceef9450a2758bcdf52999d0e44
YuriJRC[credential] <Jurassic5@ya.ru>
2017-03-22 10:07:29 +0300

    creation and class logic tests added

--- unit06/src/test/java/task02/PropertiesReaderToHashMapTest.java
+++ unit06/src/test/java/task02/PropertiesReaderToHashMapTest.java
@@ -3,20 +3,28 @@ package task02;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
 /**
  * Created by Мария on 22.03.2017.
  */
 public class PropertiesReaderToHashMapTest {
+    PropertiesReaderToHashMap readerToHashMap;
 
     @Before
     public void init(){
-
+        readerToHashMap = new PropertiesReaderToHashMap();
     }
 
 
     @Test
-    public void testThatWeCanNewHashMap() {
-
+    public void testThatWeCanCreateNewHashMap() {
+        HashMap <String, String> emptyMap = readerToHashMap.getPropertiesToHashMap("", "");
+        assertThat(emptyMap, is(notNullValue()));
     }
 
     @Test
@@ -40,7 +48,24 @@ public class PropertiesReaderToHashMapTest {
     }
 
     @Test
-    public void addKeyThatAlreadyExistsTest(){
+    public void testThatWeCanGetKeyByValue(){
 
+    }
+
+    @Test
+    public void addKeyThatAlreadyExistsTest(){
+        readerToHashMap.getPropertiesToHashMap("test", "");
+        readerToHashMap.getPropertiesToHashMap("test", "test_ru");
+
+//        assertThat();
+    }
+
+    @Test
+    public void classLogicTest(){
+        readerToHashMap.getPropertiesToHashMap("test_en_US", "en_US");
+        readerToHashMap.showAllProperties();
+
+        System.out.println();
+        System.out.println(readerToHashMap.getValueByKey("1"));
     }
 }
