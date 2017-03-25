07aa0f4578579bd52ea1bbe38be218eca4b42b5a
YuriJRC[credential] <Jurassic5@ya.ru>
2017-03-25 12:37:38 +0300

    hashCode method added, containsKey method improved. so we can put multiple keys

---
+++ custom_hashmap/src/main/java/com/epam/java/se/CustomHashMap.java
@@ -0,0 +1,114 @@
+package com.epam.java.se;
+import java.util.*;
+
+/**
+ * Created by Мария on 25.03.2017.
+ */
+public class CustomHashMap<K, V> implements Map<K, V> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private CustomEntry<K, V>[] buckets = new CustomEntry[DEFAULT_CAPACITY];
+
+    Map m = new HashMap();
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        Objects.requireNonNull(key);
+
+        int hash = hashCode((K)key);
+        CustomEntry<K, V> bucket = buckets[hash];
+        if(bucket != null){
+            return bucket.key.equals(key);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        Objects.requireNonNull(key);
+        int hash = hashCode(key);
+        buckets[hash] = new CustomEntry<>(key, value);
+
+
+        return null; //TODO implement return prev value
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    private int hashCode(K key){
+        return Math.abs(key.hashCode()) % DEFAULT_CAPACITY;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+
+    private class CustomEntry<K, V> implements Iterator<CustomEntry<K, V>> {
+
+        private final K key;
+        private V value;
+        private CustomEntry<K, V> next = null;
+
+        CustomEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public boolean hasNext() {
+            return this.next != null;
+        }
+
+        public CustomEntry<K, V> next() {
+            return this.next;
+        }
+
+        void setNext(CustomEntry<K, V> next) {
+            this.next = next;
+        }
+    }
+}
---
+++ custom_hashmap/src/test/java/com/epam/java/se/CustomHashMapTest.java
@@ -0,0 +1,113 @@
+package com.epam.java.se;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+/**
+ * Created by Мария on 25.03.2017.
+ */
+
+@FixMethodOrder
+public class CustomHashMapTest {
+
+    private Map<Integer, String> m;
+
+    @Before
+    public void init() {
+        m = new CustomHashMap<>();
+    }
+
+    @Test
+    public void testThatWeCanCreate() {
+        assertThat(m, is(notNullValue()));
+
+    }
+
+    @Test
+    public void testThatNewMapIsEmpty() {
+        assertThat(m.isEmpty(), is(true));
+    }
+
+    @Test
+    public void testThatOnNewMapContainKeyMethodReturnFalseForAnyObject() {
+        assertThat(m.containsKey(new Integer(1)), is(false));
+    }
+
+    @Test
+    public void testThatWeCanPutKeyValuePairAndCanCheckIt() {
+        m.put(new Integer(3), "abc");
+        assertThat(m.containsKey(3), is(true));
+    }
+
+    @Test
+    public void testThatWeCanPutMoreThanOneKeyValuePairAndCanCheckIt() {
+        m.put(new Integer(3), "abc");
+        m.put(new Integer(5), "abc");
+        assertThat(m.containsKey(3), is(true));
+        assertThat(m.containsKey(5), is(true));
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testThatMapCantContainNullKey() {
+        assertThat(m.containsKey(null), is(false));
+    }
+
+    @Test
+    public void testThatIfCapacityIsNotEnoughItWillIncrease() {
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThatWeCantPutNullKey() {
+        m.put(null, "abc");
+    }
+
+    @Test
+    public void testThatWeCanPutNullValue() {
+    }
+
+    @Test(expected = OutOfMemoryError.class)
+    public void testThatMapHaveInfiniteCapacity() {
+    }
+
+    @Test
+    public void testThatMapCanPutPairWithKeyThatAlreadyPresented() {
+    }
+
+    @Test
+    public void testThatMapCanContainsKeysWithSameHashCode() {
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThatContainsKeyMethodThrowsExceptionOnNullKey() {
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void testThatContainsKeyMethodThrowsExceptionOnWrongKeyClass() {
+    }
+
+    @Test
+    public void testContainsValueMethodWorksProperlyOn() {
+    }
+
+    @Test
+    public void testContainsValueMethodWorksProperlyOnNullInputValue() {
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void testValueContainsMethodThrowsExceptionOnWrongInputValueClass() {
+    }
+
+    @Test
+    public void testThatMapCalculateItsSizeProperly() {
+    }
+
+
+}
