7978ed561850d490d62a82de9ed8189ec1dc4435
YuriJRC[credential] <Jurassic5@ya.ru>
2017-03-26 14:01:19 +0300

    size() method and test added

---
+++ custom_treemap/src/main/java/com/epam/java/se/CustomTreeMap.java
@@ -0,0 +1,135 @@
+package com.epam.java.se;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Мария on 26.03.2017.
+ */
+public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K, V> {
+    private Node<K, V> root;
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        else return node.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        Objects.requireNonNull(key);
+
+        if (root == null) return false;
+        root.key.compareTo((K) key);
+        return find(root, (K) key) != null;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        if (root == null) return false;
+        if (root.value == null) {
+            return value == null;
+        } else {
+            return root.value.equals(value);
+        }
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        Objects.requireNonNull(key);
+        root = put(root, key, value);
+        return value;
+    }
+
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
+        if (node == null) {
+            return new Node<>(key, value, 1);
+        }
+        if (node.key.equals(key)) {
+            node.value = value;
+        } else if (node.key.compareTo(key) > 0) {
+            node.left = put(node.left, key, value);
+        } else {
+            node.right = put(node.right, key, value);
+        }
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    private Node<K, V> find(Node<K, V> node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (node.key.equals(key)) {
+            return node;
+        } else if (node.key.compareTo(key) > 0) {
+            return find(node.left, key);
+        } else {
+            return find(node.right, key);
+        }
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+
+    private class Node<K extends Comparable<K>, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> left;
+        private Node<K, V> right;
+        private int size;
+
+        public Node(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+
+    }
+
+}
