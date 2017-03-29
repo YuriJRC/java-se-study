92dd538a40be3e9b8bde091e5e04bd85dfba7ea8
YuriJRC[credential] <Jurassic5@ya.ru>
2017-03-29 17:15:11 +0300

    putAll() fixed

--- custom_hashmap/src/main/java/com/epam/java/se/CustomHashMap.java
+++ custom_hashmap/src/main/java/com/epam/java/se/CustomHashMap.java
@@ -202,8 +202,8 @@ public class CustomHashMap<K, V> implements Map<K, V> {
      */
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
+        Objects.requireNonNull(m);
         for (Map.Entry<? extends K, ? extends V> entry : m.entrySet()) {
-            Objects.requireNonNull(entry.getKey());
             put(entry.getKey(), entry.getValue());
         }
     }
